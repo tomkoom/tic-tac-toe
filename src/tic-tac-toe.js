818828f10cb47cf534fa0e6438c59fb4977d3586
tomkoom <tomash.sugint@gmail.com>
2021-08-30 22:31:22 +0300

    init: start tic-tac-toe task

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,14 +1,25 @@
 class TicTacToe {
+    currentPlayer = 'x';
+    gameField = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null],
+    ];
+
+
     constructor() {
 
     }
 
     getCurrentPlayerSymbol() {
-
+        return this.currentPlayer;
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        if (this.gameField[rowIndex][columnIndex] === null) {
+            this.gameField[rowIndex][columnIndex] = this.currentPlayer;
+            this.currentPlayer = this.currentPlayer === 'x' ? 'o' : 'x';
+        } else return false;
     }
 
     isFinished() {
@@ -28,7 +39,7 @@ class TicTacToe {
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        return this.gameField[rowIndex][colIndex];
     }
 }
 
